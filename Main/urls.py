0779a2802f1a0ff0c1c62d8029125c696dc4fb58
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2023-04-09 15:46:28 +0530

    Session Handle
    Session Created for users after logged in

--- Main/urls.py
+++ Main/urls.py
@@ -8,9 +8,11 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.indexPage, name="indexPage"),
-    path('render_login_page', views.render_login_page, name="render_login_page"),
+    path('login', views.render_login_page, name="render_login_page"),
     
     # Authentication
     path('userLogin', views.userLogin, name="userLogin"),
     path('verify_otp', views.verify_otp, name="verify_otp"),
+    
+    path('logoutHandle', views.logoutHandle, name="logoutHandle"),
 ]
--- Main/views.py
+++ Main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from os import *
 
@@ -56,15 +57,39 @@ def verify_otp(request):
         input_otp, email = request.POST.get('otp-input'), request.POST.get('email')
         
         user_data = Users.objects.get(email_id=email)
+        # return HttpResponse(user_data.email_id)
         
         if len(input_otp) < 6:
             er_context = {'result' : True, 'email': email, 'errormsg': "Invalid OTP! must be 6 digits"}
             return render(request, 'login.html', er_context)
         
         if int(user_data.otp) == int(input_otp):
-            return redirect(reverse('indexPage'))
+            
+            request.session['first_name'] = user_data.first_name
+            request.session['last_name'] = user_data.last_name
+            request.session['gender'] = user_data.gender
+            request.session['email_id'] = user_data.email_id
+            request.session['phone_number'] = user_data.phone_number
+            request.session['is_phone_verified'] = user_data.is_phone_verified
+            request.session['user_type'] = user_data.user_type
+            request.session['is_session'] = True
+            
+            return HttpResponseRedirect('/')
         else:
             er_context = {'result' : True, 'email': email, 'errormsg': "OTP doesn't match! New OTP sent"}
             return render(request, 'login.html', er_context)
         
-        
+def logoutHandle(request):
+    try:
+        del request.session['first_name']
+        del request.session['last_name']
+        del request.session['gender']
+        del request.session['email_id']
+        del request.session['phone_number']
+        del request.session['is_phone_verified']
+        del request.session['user_type']
+        del request.session['is_session']
+    except KeyError:
+        pass
+    # messages.success(request, "You are Logged out!")
+    return redirect(reverse('indexPage'))
