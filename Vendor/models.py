97729558813e76758cfca0344b82f7622eba4f99
Varad Joshi <100303258+VaradJoshi18@users.noreply.github.com>
2023-04-10 00:46:56 +0530

    add product

--- Vendor/models.py
+++ Vendor/models.py
@@ -1,10 +1,17 @@
+import datetime
+import os
 from django.db import models
 
+def filepath(request, filename):
+    old_filename = filename
+    timeNow = datetime.datetime.now().strftime('%Y%m%d%H:%M:%S')
+    filename = "%s%s" % (timeNow, old_filename)
+    return os.path.join('product_img/', filename)
 
 # Create your models here.
 class Vendor(models.Model):
     prodname = models.CharField(max_length=50)
     proddescription = models.CharField(max_length=500)
-    prodimage = models.ImageField(upload_to='vendor/product_img', null=True, blank=True)
+    prodimage = models.ImageField(upload_to=filepath, null=True, blank=True)
     prodprice = models.IntegerField(max_length=20)
     discount = models.CharField(max_length=50)
