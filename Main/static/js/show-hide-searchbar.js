d88010a4db943ada00baf5a3df16b9722715d598
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2023-04-17 23:54:51 +0530

    Main application
    • User Profile Edit

--- Main/static/js/show-hide-searchbar.js
+++
@@ -1,9 +0,0 @@
-function showSearchbar() {
-    var searchBarBox = document.getElementById("search-bar-box");
-    searchBarBox.classList.add("toggle-searchbar");
-}
-
-function hideSearchbar() {
-    var searchBarBox = document.getElementById("search-bar-box");
-    searchBarBox.classList.remove("toggle-searchbar");
-}
--- Main/urls.py
+++ Main/urls.py
@@ -9,9 +9,15 @@ from django.urls import path, include
 urlpatterns = [
     path('', views.indexPage, name="indexPage"),
     path('login', views.render_login_page, name="render_login_page"),
+    path('account', views.render_account_page, name="render_account_page"),
+    
     
     # Authentication
     path('user_login', views.user_login, name="user_login"),
     path('login_handle', views.login_handle, name="login_handle"),
     path('logout_handle', views.logout_handle, name="logout_handle"),
+    
+    # Update User profile
+    path('update_user_profile', views.update_user_profile, name="update_user_profile"),
+    
 ]
