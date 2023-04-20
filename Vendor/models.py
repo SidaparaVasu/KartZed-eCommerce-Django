81cf5a2b0e1d227aafc0c9406dd84b96fd3f3a67
codeonlife <bhavinkalal3118@gmail.com>
2023-04-20 16:42:23 +0530

    vendor_games

--- Vendor/models.py
+++ Vendor/models.py
@@ -1,6 +1,7 @@
 import datetime
 import os
 from django.db import models
+from Administrator.models import *
 
 def filepath(request, filename):
     old_filename = filename
@@ -9,10 +10,24 @@ def filepath(request, filename):
     return os.path.join('product_img/', filename)
 
 # Create your models here.
-class Vendor(models.Model):
-    product_key = models.AutoField(primary_key=True, max_length=16)
-    prodname = models.CharField(max_length=50)
-    proddescription = models.CharField(max_length=500)
-    prodimage = models.ImageField(upload_to=filepath, null=True, blank=True)
-    prodprice = models.CharField(max_length=20)
-    discount = models.CharField(max_length=50)
+class Games(models.Model):
+    product_key     = models.AutoField(primary_key=True, max_length=16)
+    game_developer  = models.CharField(max_length=50)
+    game_name       = models.CharField(max_length=50)
+    game_publisher  = models.CharField(max_length=50)
+    game_description= models.CharField(max_length=500)
+    platform_name   = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    game_feature    = models.ForeignKey(GameFeatures, on_delete=models.CASCADE)
+    game_modes      = models.ForeignKey(GameModes, on_delete=models.CASCADE)
+    game_category   = models.ForeignKey(GameCategory, on_delete=models.CASCADE)
+    os_name         = models.ForeignKey(OperatingSystems, on_delete=models.CASCADE)
+    os_version      = models.ForeignKey(OSVersions, on_delete=models.CASCADE)
+    processors_name = models.ForeignKey(Processors, on_delete=models.CASCADE)
+    vc_name         = models.ForeignKey(VideoCards, on_delete=models.CASCADE)
+    vc_version      = models.ForeignKey(VCVersions, on_delete=models.CASCADE)
+    game_ram        = models.CharField(max_length=10)
+    game_languages  = models.CharField(max_length=25)
+    game_regions    = models.CharField(max_length=50)
+    game_price      = models.IntegerField(max_length=5)
+    avail_stock     = models.IntegerField(max_length=5)
+    discount        = models.CharField(max_length=50)
