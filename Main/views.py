00bb14456edc16b0ae4cfd8adcc38210dbbfeaa6
Sidapara_Vasu <74949446+SidaparaVasu@users.noreply.github.com>
2023-04-22 15:22:56 +0530

    resolve Add to Cart bug
    • an error was occurred while user is not logged in.

--- Main/views.py
+++ Main/views.py
@@ -7,18 +7,22 @@ from Vendor.models import *
 from .models import *
 from Email.views import Email
 
-
 # Create your views here.
 def indexPage(request):
-    user = request.session['cust_email']
-    user_id = Customers.objects.get(cust_email = user)
-    #cart_count : Cart.objects.get(cust_id = user_id).count()
-    
-    context = {
-        'games' : Games.objects.all(),
-        'cart_count' : Cart.objects.filter(cust_id = user_id).count()
+    if 'cust_email' not in request.session:
+        context = {
+            'games' : Games.objects.all()
         }
-    return render(request, 'index.html',context)
+        return render(request, 'index.html', context)
+    else:
+        user = request.session['cust_email']
+        user_id = Customers.objects.get(cust_email = user)
+    
+        context = {
+            'games' : Games.objects.all(),
+            'cart_count' : Cart.objects.filter(cust_id = user_id).count()
+            }
+        return render(request, 'index.html',context)
 
 def render_account_page(request):
     return render(request, 'user_account.html')
@@ -27,18 +31,22 @@ def view_cart(request):
     return render(request, 'Cart/viewcart.html')
 
 def add_to_cart(request,id):
-    game = Games.objects.get(product_key = id)
-    user = request.session['cust_email']
-    user_id = Customers.objects.get(cust_email = user)
-    #return HttpResponse(user_id.cust_id)
-    try:
-        cart = Cart.objects.create(
-            cust_id = user_id,
-            is_paid = False
+    
+    if 'is_session' in request.session:
+        game = Games.objects.get(product_key = id)
+        user = request.session['cust_email']
+        user_id = Customers.objects.get(cust_email = user)
+        #return HttpResponse(user_id.cust_id)
+        try:
+            cart = Cart.objects.create(
+                cust_id = user_id,
+                is_paid = False
             )
-        cartitem = CartItems.objects.create(cart = cart, game = game)
+            cartitem = CartItems.objects.create(cart = cart, game = game)
+            
+        except Exception as e:
+            return HttpResponse(e)
+        return redirect(reverse(indexPage))
+    else:
+        return redirect(reverse('render_customer_login_page'))
         
-    except Exception as e:
-        return HttpResponse(e)
-
-    return redirect(reverse(indexPage))
