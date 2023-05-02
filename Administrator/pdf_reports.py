e493eab400aec0b4e401b69948381de4504eb0da
codeonlife <bhavinkalal3118@gmail.com>
2023-05-02 06:11:19 +0530

    pdf download done

--- Administrator/pdf_reports.py
+++ Administrator/pdf_reports.py
@@ -1,20 +1,22 @@
-from tkinter import Canvas
+from reportlab.pdfgen import canvas
+from io import BytesIO
 from urllib import response
 from django.http import HttpResponse
+from django.db.models import F
 from Vendor.models import Games
 
 def download_report(request): 
-    resonse = HttpResponse(content_type='application/pdf')
+    response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="Users.pdf"'
     # Create a new PDF document and write to the response
-    pdf = Canvas.Canvas(response)
+    pdf = canvas.Canvas(response)
     pdf.setTitle("Top 5 Costliest Games Report")
     # Define the table headers
-    headers = ['Rank','Product Key','Game Name','Game Price']
+    headers = ['Product Key','Game Name','Game Price']
     # Define the table data
     data = [[g.product_key, g.game_name, g.game_price] for g in Games.objects.order_by(F('game_price').desc())[:5]]
     # Define the width and height of each column
-    col_widths = [pdf.stringWidth(h) + 10 for h in headers]
+    col_widths = [pdf.stringWidth(h) + 180 for h in headers]
     row_height = 20
     # Draw the table headers
     for i, header in enumerate(headers):
